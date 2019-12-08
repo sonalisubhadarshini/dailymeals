573e5eb448b8f0e39ae6083e1c81369781ad3b22
sonalisubhadarshini <soonali.ray@gmail.com>
2019-12-08 14:16:13 +0530

    select meals for choosing category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './dummy_categories.dart';
 
 class CategoryMealsScreen extends StatelessWidget{
 
@@ -15,6 +16,12 @@ class CategoryMealsScreen extends StatelessWidget{
 
     final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(categoryId);
+
+    }).toList();
+
 
     // TODO: implement build
     return Scaffold(
@@ -22,11 +29,13 @@ class CategoryMealsScreen extends StatelessWidget{
            title: Text(categoryTitle),
          ),
 
-           body: Center(
-        child: Text(
-          "The Reciepes for the Category!"
-          ),
-          ),
+           body: ListView.builder(
+             itemBuilder: (ctxt, index){
+           
+           return Text(categoryMeals[index].title);
+               
+             }, itemCount: categoryMeals.length,
+           )
     );
   }
 
--- lib/model/category.dart
+++ lib/model/category.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class Categories{
+class Category{
 
   final String id;
   final String title;
   final Color color;
 
- const Categories({
+ const Category({
    @required this.id, 
    @required this.title, 
    @required this.color = Colors.orange,
